58c3613ae0c9d960209ca65936ffc13ce45ce56b
Materka11 <arekmaterka11@gmail.com>
2025-02-14 12:54:07 +0100

    Feature: Add manual seed for reproducibility and read input from file

--- biogram.py
+++ biogram.py
@@ -9,3 +9,8 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+
+torch.manual_seed(2137)
+
+with open("input.txt", 'r', encoding='utf-8') as f:
+  text = f.read()
