d8dea2b1d81369ca88e004aa289cb3fa98078267
Materka11 <arekmaterka11@gmail.com>
2025-02-18 13:05:09 +0100

    Feature: Implement Bigram Language Model with training and generation capabilities

--- biogram.py
+++ biogram.py
@@ -49,3 +49,52 @@ with torch.no_grad():
       out[split] = losses.mean()
     model.train()
     return out
+
+class BigramLangugageModel(nn.Module):
+
+  def __init__(self, vocab_size):
+    super().__init__()
+    self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+  def forward(self, idx, targets=None):
+    logits = self.token_embedding_table(idx)
+
+    if targets is None:
+      loss = None
+    else:
+      B, T, C = logits.shape
+      logits = logits.view(B*T, C)
+      targets = targets.view(B*T)
+      loss = F.cross_entropy(logits, targets)
+
+    return logits, loss
+  
+  def generate(self, idx, max_new_tokens):
+    for _ in range(max_new_tokens):
+      logits, loss = self(idx)
+      logits = logits[:, -1:]
+      probs = F.softmax(logits, dim=-1)
+      idx_next = torch.multinomial(probs, num_samples=1)
+      idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+  
+model = BigramLangugageModel(vocab_size)
+m = model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+  if iter % eval_interval == 0:
+    losses = estimate_loss()
+    print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+  
+  xb, yb = get_batch('train')
+
+  logits, loss = model(xb, yb)
+  optimizer.zero_grad(set_to_none=True)
+  loss.backward()
+  optimizer.step()
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
