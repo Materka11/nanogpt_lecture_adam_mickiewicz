250208f4120a1f624f4d99d626c73094347a7e1f
Materka11 <arekmaterka11@gmail.com>
2025-02-19 14:25:26 +0100

    Feature: Refactor BigramLanguageModel to use embedding size parameter and add linear layer for output

--- biogram.py
+++ biogram.py
@@ -9,6 +9,7 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
 
 torch.manual_seed(2137)
 
@@ -52,12 +53,14 @@ with torch.no_grad():
 
 class BigramLangugageModel(nn.Module):
 
-  def __init__(self, vocab_size):
+  def __init__(self):
     super().__init__()
-    self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+    self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+    self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
-    logits = self.token_embedding_table(idx)
+    tok_emb = self.token_embedding_table(idx)
+    logits = self.lm_head(tok_emb)
 
     if targets is None:
       loss = None
@@ -72,13 +75,13 @@ class BigramLangugageModel(nn.Module):
   def generate(self, idx, max_new_tokens):
     for _ in range(max_new_tokens):
       logits, loss = self(idx)
-      logits = logits[:, -1:]
+      logits = logits[:, -1, :]
       probs = F.softmax(logits, dim=-1)
       idx_next = torch.multinomial(probs, num_samples=1)
       idx = torch.cat((idx, idx_next), dim=1)
     return idx
   
-model = BigramLangugageModel(vocab_size)
+model = BigramLangugageModel()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
